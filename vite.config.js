067eb64b9514849ac2fe9f811efd2b5bd98f1fcb
Aina Llamas <ainallamas@gmail.com>
2022-10-18 10:38:32 +0200

    files organized

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // Cuando hagamos build -> cual será la carpeta de destino. Tenemos que poner la carpeta a la que vamos a vincular el github pages, en este caso, 'docs'. 
+  build: {outDir: 'docs'},
+  // Le vamos a decir cual es la base de todo -> vamos a crear una variable de entorno -> quiere decir si estamos en un yarn dev o un build. Producción quiere decir que ya está en el servidor y es el definitivo. Si es producción, le vamos a poner en el repositorio llamado ironhackfinalproject (tenemos que ponerle el nombre del repositorio -> hay diferencias entre las mayúsculas y las minúsculas).
+  //Se le llama operación ternario y viene a ser lo mismo que lo siguiente:  
+  // if (process.env.NODE_ENV == 'production'){
+    // base: 'produccion'
+    // } else { 
+      // base: '/'
+  // }
+  base: process.env.NODE_ENV == 'production' ? '/IronhackFinalProject' : '/',
 })
